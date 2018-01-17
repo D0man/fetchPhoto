d179f74e5329785a86c6ff0e898a2d4ddd32e0df
d0man <jakub.tomasz.domanski@gmail.com>
2018-01-17 23:01:05 +0100

    setting development environment

---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const path = require('path')
+const webpack = require('webpack')
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'build.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.less$/,
+        use: [{
+          loader: "style-loader" 
+      }, {
+          loader: "css-loader" 
+      },
+      {
+        loader: "postcss-loader" 
+      },
+      {
+          loader: "less-loader" 
+      }]
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+        name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true,
+    overlay: true
+  },
+  performance: {
+    hints: false
+  },
+  devtool: '#eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new ExtractTextPlugin({
+     filename: "./dist/[name].css",
+     disable: process.env.NODE_ENV === "some"
+   }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ])
+}
