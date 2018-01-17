b14f3b71719fa7e1246960c279427774cce6a051
d0man <jakub.tomasz.domanski@gmail.com>
2018-01-18 00:56:43 +0100

    adding fetch logic

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 import './styles/style.less'
 // import './styles/style.css'
-console.log("hello world");
+let picturesSRC = [];
+const myImage = document.querySelector('img');
+//TODO make fetch in function and in loop with parametr 0+3n
+fetch('https://picsum.photos/list')
+.then(response => response.json())
+.then(data =>test(data[0].post_url))
+.catch(() => console.log('erorr'));
+//TODO make names better
+function test (x){
+    //TODO make last instant of 4
+    x = x.split('/')[4]
+    return myImage.setAttribute('src',`http://source.unsplash.com/${x}`)
+}
