21cf324ea71a682c402838f31060662d9920e76d
d0man <jakub.tomasz.domanski@gmail.com>
2018-01-18 22:35:41 +0100

    implement fetching image

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
-import './styles/style.less'
-// import './styles/style.css'
-let picturesSRC = [];
-const myImage = document.querySelector('img');
-//TODO make fetch in function and in loop with parametr 0+3n
-fetch('https://picsum.photos/list')
-.then(response => response.json())
-.then(data =>test(data[0].post_url))
-.catch(() => console.log('erorr'));
-//TODO make names better
-function test (x){
-    //TODO make last instant of 4
-    x = x.split('/')[4]
-    return myImage.setAttribute('src',`http://source.unsplash.com/${x}`)
+import 'whatwg-fetch';
+import './styles/style.less';
+const url = "https://picsum.photos/list";
+const btn = document.querySelector('.button-next');
+let maxLimit = 3;
+let minLimit = 0;
+let index = 1;
+
+btn.addEventListener('click',() => {
+    if(index!=333){
+        index++;
+        fetchImages(index);
+    }
+    else{
+        btn.parentElement.removeChild(btn);
+    } 
+});
+//TODO make function that doesn't fetch evrytime
+function fetchImages(index) {
+    let min = minLimit + index*3;
+    let max = maxLimit + index*3;
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        data.forEach((el,i)=>{
+            if (i>=min && i<max){
+            takeId(el.post_url);
+            }
+        });
+        btn.removeAttribute('disabled');
+        })
+    .catch(() => console.log('erorr'));
 }
+function takeId (id){
+    const flexCointainer = document.querySelector('.flex');
+    const myImage = document.createElement("img");
+    id = id.split('/');
+    id = id[id.length-1];
+    myImage.classList.add('flex-image');
+    myImage.setAttribute('src',`http://source.unsplash.com/${id}`);
+    flexCointainer.appendChild(myImage);
+}
+fetchImages(index);
